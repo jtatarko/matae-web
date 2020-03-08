0cd7aa8f1eb71bc22b509abea65db7b94d1f5419
Jakub Tatarko <jakub@dcg.team>
2020-03-08 22:42:51 +0100

    initial setup

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+
+export default {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  manifest: {
+    name: 'matae.sk',
+    display: 'standalone',
+    short_name: 'matae.sk',
+    description: 'Terapie a poradenstvo',
+    start_url: '/',
+    orientation: 'portrait-primary',
+    background_color: '#ffffff',
+    theme_color: '#016C63'
+  },
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-full h-screen flex flex-col justify-center items-center">
+    <img
+      src="../assets/vesica-piscis.svg"
+      alt=""
+      class="absolute top-0 opacity-25 h-screen"
+    />
+    <div class="relative flex-1 h-full flex flex-col justify-center">
+      <img
+        src="../assets/matae-logo-emerald-light.svg"
+        alt=""
+        width="400"
+        height="400"
+        class="w-56 mx-auto"
+      />
+      <p class="text-xl mt-2 text-center">Terapie a poradenstvo</p>
+    </div>
+    <div class="text-lg my-8">
+      <!-- <a href="mailto:contact@matae.sk">contact@matae.sk</a>
+      | -->
+      <a class="underline" href="tel:+421907677074">+421 907 677 074</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'IndexPage',
+
+  components: {},
+}
+</script>
+
+<style lang="postcss" scoped></style>
